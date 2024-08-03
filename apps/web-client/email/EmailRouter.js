685e8cd6025b0b29592fee0461225651bee57dd0
pashutosh2001 <pashutosh1957@gmail.com>
2024-08-04 03:28:44 +0530

    added new form and api to reset password of Promoter(General)

--- apps/web-client/email/EmailRouter.js
+++ apps/web-client/email/EmailRouter.js
@@ -10,13 +10,14 @@ const EmailRouter = express.Router();
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',  
+  service: 'gmail',
   auth: {
-      user: process.env.GMAIL_ID,
-      pass: process.env.GMAIL_PASSWORD
+    user: process.env.GMAIL_ID,
+    pass: process.env.GMAIL_PASSWORD
   }
 });
 
+/// forgot password api for the normal user
 
 EmailRouter.post(
   "/sendMail",
@@ -27,14 +28,14 @@ EmailRouter.post(
       subject: 'Forgot Password Mail From Toss Job Service', // Subject line
       html: `${process.env.HOST}/api/email/forgotpassword`
     };
-    
+
     transporter.sendMail(mailOptions, function (err, info) {
-      if(err)
-          console.log(err)
+      if (err)
+        console.log(err)
       else
-          res.status(200).send({"message":"Link Sent"})
+        res.status(200).send({ "message": "Link Sent" })
     })
-    
+
   })
 );
 
@@ -48,22 +49,75 @@ EmailRouter.get(
 EmailRouter.post(
   "/saved",
   expressAsyncHandler(async (req, res) => {
-    const findUser = await SignUpModel.exists({email:req.body.email})
-    if(findUser){
-      if(req.body.password !== req.body.confirm_password){
-        res.status(400).send({"message":"Password do not match"})
-      }else{
-        
-        await SignUpModel.findOneAndUpdate({"email":req.body.email},{"password":bcrypt.hashSync(req.body.password, 8),}).exec(function(err,mess){
-          if(err){
-            res.status(400).send({"message":err})
-          }else{
-            res.status(200).send({"message":"Password Updated Successfully"})
+    const findUser = await SignUpModel.exists({ email: req.body.email })
+    if (findUser) {
+      if (req.body.password !== req.body.confirm_password) {
+        res.status(400).send({ "message": "Password do not match" })
+      } else {
+
+        await SignUpModel.findOneAndUpdate({ "email": req.body.email }, { "password": bcrypt.hashSync(req.body.password, 8), }).exec(function (err, mess) {
+          if (err) {
+            res.status(400).send({ "message": err })
+          } else {
+            res.status(200).send({ "message": "Password Updated Successfully" })
           }
         })
 
       }
-    }else{
+    } else {
+      res.send("This User Does not exists. Please Create a Profile First")
+    }
+  })
+);
+////
+
+/// forgot password api for the GENERAL
+EmailRouter.post(
+  "/sendMailPromoter",
+  expressAsyncHandler(async (req, res) => {
+    const mailOptions = {
+      from: process.env.GMAIL_ID, // sender address
+      to: req.body.email, // list of receivers
+      subject: 'Forgot Password Mail From Toss Job Service', // Subject line
+      html: `${process.env.HOST}/api/email/forgotpasswordPromoter`
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err)
+        console.log(err)
+      else
+        res.status(200).send({ "message": "Link Sent" })
+    })
+
+  })
+);
+
+EmailRouter.get(
+  "/forgotpasswordPromoter",
+  expressAsyncHandler(async (req, res) => {
+    res.render("formPromoter")
+  })
+);
+
+EmailRouter.post(
+  "/savedPromoter",
+  expressAsyncHandler(async (req, res) => {
+    const findUser = await PromoterSignUpModel.exists({ email: req.body.email })
+    if (findUser) {
+      if (req.body.password !== req.body.confirm_password) {
+        res.status(400).send({ "message": "Password do not match" })
+      } else {
+
+        await PromoterSignUpModel.findOneAndUpdate({ "email": req.body.email }, { "password": bcrypt.hashSync(req.body.password, 8), }).exec(function (err, mess) {
+          if (err) {
+            res.status(400).send({ "message": err })
+          } else {
+            res.status(200).send({ "message": "Password Updated Successfully" })
+          }
+        })
+
+      }
+    } else {
       res.send("This User Does not exists. Please Create a Profile First")
     }
   })
