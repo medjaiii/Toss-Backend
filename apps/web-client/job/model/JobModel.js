fa5c46d7d013d9441f603bcf9f87ffc2f715a2c0
pashutosh2001 <pashutosh1957@gmail.com>
2024-08-04 21:50:07 +0530

    added recipient contact in the new job schema

--- apps/web-client/job/model/JobModel.js
+++ apps/web-client/job/model/JobModel.js
@@ -2,32 +2,35 @@ import Mongoose from "mongoose";
 
 const jobSchema = new Mongoose.Schema(
   {
-    job_name: { type: String, required: [true, "name required"] },    
+    job_name: { type: String, required: [true, "name required"] },
     recepient: {
+      type: String,
+    },
+    recepientContact: {
       type: String,
     },
     company_Name: {
       type: String,
     },
-    description :{
-        type:String,
+    description: {
+      type: String,
     },
     requirement: { type: String },
-    workers: { type: Number},
+    workers: { type: Number },
     address: { type: String },
     pincode: { type: String },
-    duration:{
-        startDuration:{type:String},
-        endDuration:{type:String},
+    duration: {
+      startDuration: { type: String },
+      endDuration: { type: String },
     },
-    job_images:{ type: String},
-    posted_by:{type: Mongoose.Schema.Types.ObjectId,ref:"PromoterSignUp"},
-    payment:{type:Number,default:500},
-    createdDate:{
-      type:Date,
-      default:Date.now
+    job_images: { type: String },
+    posted_by: { type: Mongoose.Schema.Types.ObjectId, ref: "PromoterSignUp" },
+    payment: { type: Number, default: 500 },
+    createdDate: {
+      type: Date,
+      default: Date.now
     },
-    job_status:{type:String,default:"OPEN"}
+    job_status: { type: String, default: "OPEN" }
   },
   {
     timestamps: true,
