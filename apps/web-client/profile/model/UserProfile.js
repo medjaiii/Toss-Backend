54b1825987c3a10459f7e223f1abcbc42a51c19f
pashutosh2001 <pashutosh1957@gmail.com>
2024-08-04 20:46:58 +0530

    added new date_of_birth section to the api of editProfile

--- apps/web-client/profile/model/UserProfile.js
+++ apps/web-client/profile/model/UserProfile.js
@@ -8,42 +8,43 @@ const UserSignUp = mongoose.Schema({
     type: String,
     required: [true, "Name Required"],
   },
-  email:{
-    type:String,
-    required:[true,"email required"],
+  email: {
+    type: String,
+    required: [true, "email required"],
 
   },
-  contactNumber:{
-    type:Number,
-    required:[true,"phone number required"],
+  contactNumber: {
+    type: Number,
+    required: [true, "phone number required"],
   },
-  city:{
-    type:String,
-    required:[true,"city required"],
+  city: {
+    type: String,
+    required: [true, "city required"],
   },
-  area:{
-    type:String,
-    required:[true,"area required"],
+  area: {
+    type: String,
+    required: [true, "area required"],
 
   },
-  about:{
-    type:String,
-    required:[false],
-    maxlength:100,
+  about: {
+    type: String,
+    required: [false],
+    maxlength: 100,
   },
-  previousExpereince:{
-    type:String,
-    required:[false],
+  previousExpereince: {
+    type: String,
+    required: [false],
   },
-  height:{type:String,required:[false]},
+  height: { type: String, required: [false] },
+  date_of_birth: { type: String, required: [false] },
   skills: {
     type: [{
-        name : String,
+      name: String,
     }],
     default: undefined
-},
-  profileImages:{ type: mongoose.Schema.Types.ObjectId,ref :"UserProfileImages"},
-  approvedStatus:{type:String,default:"Unapproved"}
+  },
+  profileImages: { type: mongoose.Schema.Types.ObjectId, ref: "UserProfileImages" },
+  approvedStatus: { type: String, default: "Unapproved" }
 });
 
 mongoose.pluralize(null)
