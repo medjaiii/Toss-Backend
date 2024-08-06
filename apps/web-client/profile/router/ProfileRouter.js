253d77a8116daac544aedd2958537766bfd74d29
pashutosh2001 <pashutosh1957@gmail.com>
2024-08-07 00:40:30 +0530

    added new api to delete user account

--- apps/web-client/profile/router/ProfileRouter.js
+++ apps/web-client/profile/router/ProfileRouter.js
@@ -318,7 +318,7 @@ UserProfileRouter.delete("/deleteuser", isAdminAuth, expressAsyncHandler(async (
     res.status(400).send({ "message": 'User not found' });
   } else {
     await job.remove()
-    res.status(400).send({ "message": "User Deleted" })
+    res.status(200).send({ "message": "Account Deleted Successfully" })
   }
 
 }))
